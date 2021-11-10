3170ee60b007c5acaae3efe91e29cdae96f3f4e8
John Behnke <john@behnke.xyz>
2021-11-10 18:38:42 -0500

    Initial Commit

---
+++ Shared/OrchardApp.swift
@@ -0,0 +1,20 @@
+//
+//  OrchardApp.swift
+//  Shared
+//
+//  Created by John Behnke on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct OrchardApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
